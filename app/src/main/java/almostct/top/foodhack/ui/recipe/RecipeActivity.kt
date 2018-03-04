e4bce081447ada0d6b8acfc2e622b002d7adefa3
Leonid Startsev <sandwwraith@gmail.com>
2018-03-04 07:38:50 +0300

    Fokken margins

--- app/src/main/java/almostct/top/foodhack/ui/recipe/RecipeActivity.kt
+++ app/src/main/java/almostct/top/foodhack/ui/recipe/RecipeActivity.kt
@@ -3,17 +3,26 @@ package almostct.top.foodhack.ui.recipe
 import activitystarter.Arg
 import almostct.top.foodhack.R
 import almostct.top.foodhack.model.DummyData
+import almostct.top.foodhack.model.Receipt
+import almostct.top.foodhack.ui.comments.CommentsActivity
 import almostct.top.foodhack.ui.common.InjectableActivity
 import almostct.top.foodhack.ui.cooking.CookingActivity2Starter
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.Toolbar
+import android.text.Html
 import android.text.TextUtils
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
 import com.marcinmoskala.activitystarter.argExtra
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.content_recipe.*
 import kotterknife.bindView
 
 class RecipeActivity() : InjectableActivity() {
@@ -42,7 +51,8 @@ class RecipeActivity() : InjectableActivity() {
         }
 //        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        updateUI()
+        fetchUI(recipeId)
+        recipe_open_comments.setOnClickListener { startActivity(Intent(this, CommentsActivity::class.java)) }
     }
 
     override fun getActivityTitle(): String {
@@ -60,19 +70,30 @@ class RecipeActivity() : InjectableActivity() {
         true
     } else false
 
-    fun updateUI() {
+    fun fetchUI(receiptId: String) {
+        Observable.just(DummyData.pancake)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ updateUI(it) }, { Log.e("RECEIPT", "fetch succed", it) })
+    }
+
+    fun updateUI(receipt: Receipt) {
         val txt = TextUtils.concat(
             resources.getText(R.string.recipe_general_info_cooktime),
             "2 hours",
             "\n",
             resources.getText(R.string.recipe_general_info_weight),
-            "400g"
+            "${receipt.weight}"
         )
         generalText.text = txt
-        caloriesText.text = "400"
-        bText.text = "10"
-        jText.text = "20"
-        uText.text = "50"
-        descriptionView.text = "Foo bar baz Ugandian knukles"
+        caloriesText.text = receipt.calories
+        bText.text = receipt.proteins
+        jText.text = receipt.fats
+        uText.text = receipt.carbohydrates
+        descriptionView.text = receipt.tools
+        recipe_steps.text = receipt.steps.joinToString(separator = "\n") { it.shortDescription }
+        val s1 = Html.fromHtml("<b>User1</b> wow")
+        val s2 = Html.fromHtml("<b>User2</b> foo")
+        recipe_comments.text = TextUtils.concat(s1, "\n", s2)
     }
 }
